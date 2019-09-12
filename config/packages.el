e35b7015c214b5daa2510d5219d46f1c5b865219
Isak Ernstig <ernstig1@gmail.com>
2019-09-12 16:28:25 -0400

    some cleaning up of the initial configuration

--- config/packages.el
+++ config/packages.el
@@ -1,3 +1,23 @@
+
+(setq package-enable-at-startup nil) ; tells emacs not to load any packages before starting up
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "https://melpa.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+(package-initialize)
+
+;; --------------------------------------------------
+;;            Bootstrap 'use-package'
+;; --------------------------------------------------
+
+(unless (package-installed-p 'use-package) ;; unless it is already installed
+  (package-refresh-contents) ;; update packages archive
+  (package-install 'use-package)) ;; install the most recent version of use-package
+
+;; --------------------------------------------------
+;;           Packages installation list 
+;; --------------------------------------------------
+
 (use-package general :ensure t
 	     :config
 	     (general-define-key "C-'" 'avy-goto-word-1))
@@ -8,16 +28,19 @@
 (use-package ivy :ensure t)
 (use-package counsel :ensure t)
 (use-package swiper :ensure t)
-(use-package evil :ensure t)
+
+(use-package evil :ensure t) ;; for awesome navigation 
 (use-package which-key :ensure t)
 
-(require 'ivy)
-(ivy-mode)
 
+;; --------------------------------------------------
+;;           Enable all the packages
+;; --------------------------------------------------
 
 (require 'use-package)
+(require 'ivy)
+(ivy-mode)
 (require 'evil)
 (evil-mode 1)
-
 (require 'which-key)
 (which-key-mode)
--- init.el
+++ init.el
@@ -1,45 +1,10 @@
 ;; Emacs configuration file
+;; Basically bootstrap use-package and load other configuration files
 
 ;; add scripts to load path
 (add-to-list 'load-path "~/.emacs.d/config")
-(load "settings")
-
-(setq package-enable-at-startup nil) ; tells emacs not to load any packages before starting up
-;; the following lines tell emacs where on the internet to look up
-;; for new packages.
-(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
-                         ("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("melpa"     . "https://melpa.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
-(package-initialize)
+(load "settings") ;; for setting variables and functions inherent to emacs
+(load "packages") ;; settings for all external packages
+(load "keybindings") ;; all keybinding settings
 
 
-;; add default dir for opening files
-
-
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package) ; unless it is already installed
-  (package-refresh-contents) ; updage packages archive
-  (package-install 'use-package)) ; and install the most recent version of use-package
-
-
-(load "packages.el")
-(load "keybindings.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" default)))
- '(package-selected-packages
-   (quote
-    (solarized-theme counsel ivy avy general use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
