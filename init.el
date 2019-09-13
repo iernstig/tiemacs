7fd456a37a7174ac850e8618f4f57a9843bd713a
Isak Ernstig <ernstig1@gmail.com>
2019-09-13 10:41:44 -0400

    auto added code by installation, and load appearance

--- init.el
+++ init.el
@@ -6,5 +6,20 @@
 (load "settings") ;; for setting variables and functions inherent to emacs
 (load "packages") ;; settings for all external packages
 (load "keybindings") ;; all keybinding settings
+(load "appearance")
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dracula-theme doom-modeline all-the-icons which-key use-package solarized-theme general evil counsel ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
