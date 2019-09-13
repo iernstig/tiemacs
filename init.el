0f2c412d6fb2ac20c11eac9ab191ef1819ed3f7f
Isak Ernstig <ernstig1@gmail.com>
2019-09-13 16:34:18 -0400

    no-littering added to init.el

--- init.el
+++ init.el
@@ -1,8 +1,10 @@
 ;; Emacs configuration file
 ;; Basically bootstrap use-package and load other configuration files
 
+
 ;; add scripts to load path
 (add-to-list 'load-path "~/.emacs.d/config")
+(require 'no-littering)
 (load "settings") ;; for setting variables and functions inherent to emacs
 (load "packages") ;; settings for all external packages
 (load "keybindings") ;; all keybinding settings
