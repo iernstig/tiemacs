04390edcddb16677cfa67332ca22320813471396
Isak Ernstig <ernstig1@gmail.com>
2019-09-13 19:45:38 -0400

    removed useless mode-configuration file

--- init.el
+++ init.el
@@ -9,20 +9,6 @@
 (load "packages") ;; settings for all external packages
 (load "keybindings") ;; all keybinding settings
 (load "appearance")
-(load "mode-configuration")
+(load "org-mode")
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (org-bullets projectile which-key use-package solarized-theme one-themes general evil dracula-theme doom-modeline counsel atom-one-dark-theme ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
