65c19cfe1f803875b4de64af24511cb3db48ac9a
Isak Ernstig <ernstig1@gmail.com>
2019-09-14 08:25:02 -0400

    added auto-break line at 80 col width in org

--- config/org-mode.el
+++ config/org-mode.el
@@ -1,3 +1,7 @@
 (use-package org-bullets :ensure t)
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda() (org-bullets-mode 1)))
+
+;; auto break lines at 80
+(add-hook 'org-mode-hook '(lambda () (setq fill-column 80)))
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
