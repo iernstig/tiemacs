aa1b599672167942f6b754f31adb83f7ed6067a0
Isak Ernstig <ernstig1@gmail.com>
2019-09-14 22:38:34 -0400

    cleaned up in todo list

--- config/org-mode.el
+++ config/org-mode.el
@@ -6,4 +6,13 @@
 (add-hook 'org-mode-hook '(lambda () (setq fill-column 80)))
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 
-(use-package evil-org :ensure t) 
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
