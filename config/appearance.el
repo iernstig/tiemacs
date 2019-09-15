f939cc39f973b0bc0f3ff107338c4d005297d916
Isak Ernstig <ernstig1@gmail.com>
2019-09-14 22:24:51 -0400

    added doom themes, not yet working with solarize mode or what it's called

--- config/appearance.el
+++ config/appearance.el
@@ -4,10 +4,13 @@
 (add-to-list 'default-frame-alist
              '(vertical-scroll-bars . nil))
 (tool-bar-mode -1)
+;; remove titlebar text
+(setq frame-title-format nil) 
 
-;; set the font
-;;(set-frame-font "Fira Code 10" nil t) 
+;; Display line numbers, of course!
+(global-display-line-numbers-mode) 
 
+;; set font, adjust size according to screen size
 (defun set-font (fontname fontsize)
   (set-face-attribute 'default nil :font
 		      (format "%s:pixelsize=%d" fontname fontsize)))
@@ -16,21 +19,60 @@
   (if (> (x-display-pixel-width) 2000)
 	 (set-font "Fira Code" 18)
 	 (set-font "Fira Code" 12)))
-;; load icons, modeline and theme 
+
+;; ---------------------------------------- 
+;;         Appearance packages  
+;; ---------------------------------------- 
+
+;; load icons
 (use-package all-the-icons :ensure t)
+;; modeline
 (use-package doom-modeline :ensure t)
-(use-package atom-one-dark-theme :ensure t) 
+(require 'doom-modeline)
+(doom-modeline-mode 1)
+;; and theme
+;; (use-package atom-one-dark-theme :ensure t) 
+;; (load-theme 'atom-one-dark t) 
 
-(load-theme 'atom-one-dark t) 
-(global-display-line-numbers-mode) 
-;; remove titlebar text
-(setq frame-title-format nil) 
+(use-package doom-themes :ensure t)
+(load-theme 'doom-solarized-dark t)
+
+(use-package sublimity :ensure t)
+(require 'sublimity-attractive)
+
+;;(sublimity-attractive-hide-vertical-border)
+(sublimity-attractive-hide-fringes)
+(setq sublimity-attractive-centering-width 120)
+(require 'sublimity-scroll)
+(setq sublimity-scroll-weight 100
+      sublimity-scroll-drift-lenght 100)
+(sublimity-mode 1)
+
+;; for colors, html etc.
+(use-package rainbow-mode :ensure t)
+(rainbow-mode +1)
+
+;; enable easier parenthesis reading
+(use-package rainbow-delimiters :ensure t)
+(rainbow-delimiters-mode +1) 
+
+;; easier visuals from searching
+(use-package visual-regexp-steroids :ensure t)
+
+;; for pretty side bars!
+(use-package solaire-mode :ensure t)
+
+;; fancy tabs to be aware of started buffers! 
+(use-package centaur-tabs :ensure t)
+(require 'centaur-tabs)
+(centaur-tabs-mode 1)
+(setq centaur-tabs-set-icons t)
+(centaur-tabs-headline-match)
+(setq centaur-tabs-style "bar")
+(setq centaur-tabs-set-bar 'left)
+(centaur-tabs-group-by-projectile-project)
 
 
-
-
-
-
-
-
-
+;; for easier spotting of variables
+(use-package color-identifiers-mode :ensure t)
+(color-identifiers-mode t)
