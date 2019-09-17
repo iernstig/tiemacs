8611a619fd76ccc9b6b26d31e1c0b274f9a787b7
Isak Ernstig <ernstig1@gmail.com>
2019-09-17 13:41:26 -0400

    determine which operating system

--- config/settings.el
+++ config/settings.el
@@ -2,11 +2,11 @@
 (setq delete-old-versions -1 )		; delete excess backup versions silently
 (setq version-control t )		; use version control
 (setq vc-make-backup-files t )		; make backups file even when in version controlled dir
-(setq backup-directory-alist `(("." . "~/.emacs.d/backups")) ) ; which directory to put backups file
-(setq vc-follow-symlinks t )				       ; don't ask for confirmation when opening symlinked file
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))  ; which directory to put backups file
+(setq vc-follow-symlinks t)			       ; don't ask for confirmation when opening symlinked file
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)) ) ;transform backups file name
 (setq inhibit-startup-screen t )	; inhibit useless and old-school startup screen
-(setq ring-bell-function 'ignore )	; silent bell when you make a mistake
+    (setq ring-bell-function 'ignore )	; silent bell when you make a mistake
 (setq coding-system-for-read 'utf-8 )	; use utf-8 by default
 (setq coding-system-for-write 'utf-8 )
 (setq sentence-end-double-space nil)	; sentence SHOULD end with only a point.
@@ -21,3 +21,11 @@
 
 ;; set the default dir to user home
 (setq default-directory "~/")
+
+;; some differences regarding which operating system in use
+;;* TODO 
+(cond ((string-equal system-type "windows-nt")
+       (message "Microsoft Windows"))
+      (string-equal system-type "darwin")
+      (message "OS X")
+	(string-equal system-type "gnu/linux"))
