f5964cdcabf6f40211d372e02b3d68167b3fa3cd
Isak Ernstig <ernstig1@gmail.com>
2019-09-17 22:22:18 -0400

    added module configuration directory

--- init.el
+++ init.el
@@ -4,12 +4,13 @@
 
 ;; add scripts to load path
 (add-to-list 'load-path "~/.emacs.d/config")
+(add-to-list 'load-path "~/.emacs.d/config/module-configuration")
 (require 'no-littering)
 (load "settings") ;; for setting variables and functions inherent to emacs
 (load "packages") ;; settings for all external packages
 (load "keybindings") ;; all keybinding settings
 (load "appearance")
-(load "org-mode")
+(load "org-mode") 
 (load "treemacs")
 
 
