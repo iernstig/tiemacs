00901a27b40c6fd388d092767e73142fd85a4c58
Isak Ernstig <ernstig1@gmail.com>
2019-09-21 10:28:52 -0400

    rm evil-org, in favor of own keybinds

--- config/module-configuration/org-mode.el
+++ config/module-configuration/org-mode.el
@@ -5,14 +5,4 @@
 (add-hook 'org-mode-hook '(lambda () (setq fill-column 80)))
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 
-(use-package evil-org
-  :ensure t
-  :after org
-  :config
-  (add-hook 'org-mode-hook 'evil-org-mode)
-  (add-hook 'evil-org-mode-hook
-            (lambda ()
-              (evil-org-set-key-theme)))
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
 
