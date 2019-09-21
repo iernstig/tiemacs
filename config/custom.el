6a3b216ff5d04447ebf89e4800a6af1d45d8f7e2
Isak Ernstig <ernstig1@gmail.com>
2019-09-20 22:13:56 -0400

    some auto-gen shit

--- config/custom.el
+++ config/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" default)))
+    ("428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" "5e0b63e0373472b2e1cf1ebcc27058a683166ab544ef701a6e7f2a9f33a23726" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" default)))
  '(package-selected-packages
    (quote
     (diff-hl outorg quelpa-use-package outshine-mode highlight-indent-guides highlight-indentation poporg outshine flycheck doom-themes-treemacs-configuration all-the-icons-ivy evil-magit yasnippet treemacs-magit treemacs-icons-dired use-package-hydra ivy-hydra lsp-treemacs treemacs-projectile treemacs-evil helpful dired-sidebar evil-org evil-org-mode hungry-delete hungry-delet treemacs ibuffer-sidebar color-identifiers-mode centaur-tabs lsp-ui sublimity doom-themes solaire-mode visual-regexp-steroids focus focus-mode rainbow-delimiters rainbow-mode sr-speedbar anzu restart-emacs org-bullets atom-one-dark-theme beacon dashboard lsp-mode doom-modeline projectile which-key evil counsel ivy avy general use-package))))
