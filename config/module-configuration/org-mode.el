25e295bbdd50d5d69514891cff2ef74167b984e5
Isak Ernstig <ernstig1@gmail.com>
2019-09-21 15:06:11 -0400

    added todo-states and heading+todo for outshine function

--- config/module-configuration/org-mode.el
+++ config/module-configuration/org-mode.el
@@ -5,4 +5,17 @@
 (add-hook 'org-mode-hook '(lambda () (setq fill-column 80)))
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "|" "DONE(d)")
+        (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")))
+
+(defun outshine-heading-and-todo ()
+  (interactive)
+  (progn (insert ";;; ")
+	 (beginning-of-line)
+	 (outshine-todo)
+	 (end-of-line)
+	 (insert " ")
+	 (end-of-line)
+	 ))
 
