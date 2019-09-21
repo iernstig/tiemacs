2e3c635d4f88dfeeda9b4e762d58213506dee208
Isak Ernstig <ernstig1@gmail.com>
2019-09-20 22:13:17 -0400

    restructured file according to org-outshine mode

--- config/packages.el
+++ config/packages.el
@@ -1,6 +1,5 @@
 ;; ------------------------------------------------------------
-;;           general package definition, configuration
-;;                and bootstrap of use-package
+;; * General package definition, configuration
 ;; ------------------------------------------------------------
 
 (setq package-enable-at-startup nil) ; tells emacs not to load any packages before starting up
@@ -10,7 +9,7 @@
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
 ;; ------------------------------------------------------------
-;;            Bootstrap 'use-package'
+;; * Bootstrap 'use-package'
 ;; ------------------------------------------------------------
 
 (unless (package-installed-p 'use-package) ;; unless it is already installed
@@ -18,24 +17,31 @@
   (package-install 'use-package)) ;; install the most recent version of use-package
 
 ;; ------------------------------------------------------------
-;;           Refresh package contents 
+;; * Install Quelpa-use-package
+;; ------------------------------------------------------------
+(use-package quelpa-use-package :ensure t)
+
+;; ------------------------------------------------------------
+;; * Refresh package contents 
 ;; ------------------------------------------------------------
 (when (not package-archive-contents)
   (package-refresh-contents))
 
 ;; ------------------------------------------------------------
-;;           Install Quelpa-use-package
+;; * Enable outshine mode in all programming buffers
 ;; ------------------------------------------------------------
-(use-package quelpa-use-package :ensure t)
-
 (use-package outshine
   :quelpa (outshine :fetcher github :repo "alphapapa/outshine"))
-(add-hook 'emacs-lisp-mode 'outshine-mode)
+
+(add-hook 'prog-mode-hook 'outshine-mode) ;; enable for all programming languages
+;; ugly hack to override default evil bind for elisp
+(define-key emacs-lisp-mode-map (kbd "<tab>") 'org-cycle) 
+
 ;; ------------------------------------------------------------
-;; * Packages installation list 
+;; * Install and enable packages 
 ;; ------------------------------------------------------------
 
-;; for keybindings
+;; ** Packages for editor navigation
 (use-package general :ensure t) 
 ;; Jump to things, not currently used though
 (use-package avy :ensure t) 
@@ -49,56 +55,35 @@
 (evil-mode 1)
 (use-package which-key :ensure t) ;; describe keybingings
 (which-key-mode)
-
 (use-package projectile :ensure t) ;; project management
 (projectile-mode 1)
-
 (use-package flycheck :ensure t)
-
 (use-package lsp-mode :ensure t) ;; for intelligent code analysis, debugging etc.
 (use-package lsp-ui :ensure t)
 
+;; ** Version control
 (use-package magit :ensure t)
 (use-package evil-magit :ensure t)
 (use-package diff-hl :ensure t) ; highlight diffs 
+(diff-hl-mode)
 (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 
+(use-package treemacs :ensure t) ;; tree like file view
+(use-package lsp-treemacs :ensure t)
+(use-package treemacs-evil :ensure t)
+(use-package treemacs-magit :ensure t)
+(use-package treemacs-projectile :ensure t)
+
+;; ** Snippets 
 (use-package yasnippet :ensure t)
-
-;; (use-package treemacs :ensure t) ;; tree like file view
-;; (use-package lsp-treemacs :ensure t)
-;; (use-package treemacs-evil :ensure t)
-;; (use-package treemacs-magit :ensure t)
-;; (use-package treemacs-projectile :ensure t)
-
-(use-package dashboard :ensure t ;; start dashboard 
-  :config
-  (dashboard-setup-startup-hook))
-(use-package beacon :ensure t) ;; for showing the cursor in new buffers
-(use-package restart-emacs :ensure t) ;; simple restart of emacs
-(use-package hungry-delete :ensure t) ;; delete needless space in files
-;;(use-package desktop+ :ensure t)
-;;(use-package anzu :ensure t)
-
-
-;; --------------------------------------------------
-;;           Enable all the packages
-;; --------------------------------------------------
-
-(beacon-mode 1)
-(global-hungry-delete-mode)
 (yas-global-mode 1)
 
-
+;; ** Misc useful functions
+(use-package restart-emacs :ensure t) ;; simple restart of emacs
+;;(use-package desktop+ :ensure t)
+;;(use-package anzu :ensure t)
 ;; --------------------------------------------------
 ;;           Package configuration section
 ;; --------------------------------------------------
 
-;; dashboard
-(setq dashboard-banner-logo-title "Welcome to TieMacs")
-(setq dashboard-items '((recents .5)
-			(bookmarks . 5)
-			(projects . 5)
-			(agenda . 5)
-			(registers . 5)))
 
