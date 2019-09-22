4f3349c915b06002429ae0b5c912c0b8cf615048
Isak Ernstig <ernstig1@gmail.com>
2019-09-22 11:50:00 -0400

    added outshine comments

--- config/module-configuration/org-mode.el
+++ config/module-configuration/org-mode.el
@@ -1,10 +1,12 @@
+;;; Use org-bullets
 (use-package org-bullets :ensure t)
 (add-hook 'org-mode-hook (lambda() (org-bullets-mode 1)))
 
-;; auto break lines at 80
+;;; set auto break lines at 80
 (add-hook 'org-mode-hook '(lambda () (setq fill-column 80)))
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 
+;;; Set org todo keywords
 (setq org-todo-keywords
       '((sequence "TODO(t)" "IN-PRORGESS(p)" "|" "DONE(d)") 
 	(sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")))
@@ -22,7 +24,9 @@
 
 (setq org-log-done 'time) 
 (setq org-hierarchical-todo-statistics t)
+;;; Org-capture templates
 
+;;; Restart org for changes to take effect
 (org-mode-restart)
 
 ;;; Flycheck compilation settings
