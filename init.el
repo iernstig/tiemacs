d8fce44ca19127417aec31eb95fcc3a3c8f79b47
Isak Ernstig <ernstig1@gmail.com>
2019-09-22 09:42:46 -0400

    flycheck compilation settings

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; add scripts to load path
 (add-to-list 'load-path "~/.emacs.d/config")
 (add-to-list 'load-path "~/.emacs.d/config/module-configuration")
-(setq flycheck-emacs-lisp-load-path 'inherit) ;; So that flycheck finds the files emacs 
+(setq flycheck-emacs-lisp-load-path 'inherit) ;;; So that flycheck finds the files emacs 
 (require 'no-littering)
 (load "settings") ;; for setting variables and functions inherent to emacs
 (load "packages") ;; settings for all external packages
@@ -13,5 +13,8 @@
 (load "appearance")
 (load "org-mode") 
 (load "treemacs")
-
+;;; Flycheck compilation settings
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
 
