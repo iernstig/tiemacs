e970e0806e1c7c51308829eac38cc9a516b6298d
Isak Ernstig <ernstig1@gmail.com>
2019-09-22 10:54:51 -0400

    added new org-keywords

--- config/module-configuration/org-mode.el
+++ config/module-configuration/org-mode.el
@@ -5,9 +5,9 @@
 (add-hook 'org-mode-hook '(lambda () (setq fill-column 80)))
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 
-(defvar org-todo-keywords
-  '((sequence "TODO(t)" "IN-PRORGESS(p)" "|" "DONE(d)") 
-    (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")))
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "IN-PRORGESS(p)" "|" "DONE(d)") 
+	(sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")))
 
 ;;; Outshine insert todo-heading
 (defun outshine-insert-todo-heading ()
@@ -22,6 +22,8 @@
 
 (setq org-log-done 'time) 
 (setq org-hierarchical-todo-statistics t)
+
+(org-mode-restart)
 
 ;;; Flycheck compilation settings
 ;; Local Variables:
