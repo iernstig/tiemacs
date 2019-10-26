f9598fcff3f2bd276423b4ce1a54f369bdeb271c
Isak Ernstig <ernstig1@gmail.com>
2019-10-26 13:55:48 -0400

    some config auto generation

--- config/custom.el
+++ config/custom.el
@@ -6,13 +6,24 @@
  '(custom-safe-themes
    (quote
     ("428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" "5e0b63e0373472b2e1cf1ebcc27058a683166ab544ef701a6e7f2a9f33a23726" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" default)))
+ '(lsp-auto-guess-root nil)
+ '(lsp-prefer-flymake nil)
+ '(lsp-ui-doc-border "#ffffff" t)
+ '(lsp-ui-doc-enable t t)
+ '(lsp-ui-doc-header t t)
+ '(lsp-ui-doc-include-signature t t)
+ '(lsp-ui-doc-position (quote top) t)
+ '(lsp-ui-sideline-enable nil t)
+ '(lsp-ui-sideline-ignore-duplicate t t)
+ '(lsp-ui-sideline-show-code-actions nil t)
  '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (company-lsp company dap-mode org-pomodoro org-super-agenda org-kanban org-journal navi-mode shackle aggressive-indent agressive-indent diff-hl outorg quelpa-use-package outshine-mode highlight-indent-guides highlight-indentation poporg outshine flycheck doom-themes-treemacs-configuration all-the-icons-ivy evil-magit yasnippet treemacs-magit treemacs-icons-dired use-package-hydra ivy-hydra lsp-treemacs treemacs-projectile treemacs-evil helpful dired-sidebar evil-org evil-org-mode hungry-delete hungry-delet treemacs ibuffer-sidebar color-identifiers-mode centaur-tabs lsp-ui sublimity doom-themes solaire-mode visual-regexp-steroids focus focus-mode rainbow-delimiters rainbow-mode sr-speedbar anzu restart-emacs org-bullets atom-one-dark-theme beacon dashboard lsp-mode doom-modeline projectile which-key evil counsel ivy avy general use-package))))
+    (moody ccls counsel lsp-ivy ivy company-c-headers company-cmake company-lsp company dap-mode org-pomodoro org-super-agenda org-kanban org-journal navi-mode shackle aggressive-indent agressive-indent diff-hl outorg quelpa-use-package outshine-mode highlight-indent-guides highlight-indentation poporg outshine flycheck doom-themes-treemacs-configuration evil-magit yasnippet treemacs-magit treemacs-icons-dired use-package-hydra lsp-treemacs treemacs-projectile treemacs-evil helpful dired-sidebar evil-org evil-org-mode hungry-delete hungry-delet treemacs ibuffer-sidebar color-identifiers-mode centaur-tabs lsp-ui sublimity doom-themes solaire-mode visual-regexp-steroids focus focus-mode rainbow-delimiters rainbow-mode sr-speedbar anzu restart-emacs org-bullets atom-one-dark-theme beacon dashboard lsp-mode doom-modeline projectile which-key evil avy general use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(lsp-ui-doc-background ((t (:background nil))))
+ '(lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic))))))
